54b2017e28913f38f701644995a93a758d324b62
ahir-ankit93 <ahir.ankit93@gmail.com>
2019-09-16 14:50:15 +0530

    worldgraph map chart, foundblock chart and hashrate chart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {WorldMapGraphComponent} from './world-map-graph/world-map-graph.component';
+import { WorldMapGraphComponent } from './world-map-graph/world-map-graph.component';
+import { HashrateChartComponent } from './hashrate-chart/hashrate-chart.component';
+import { FoundBlocksChartComponent } from './found-blocks-chart/found-blocks-chart.component';
 
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [ RouterModule.forRoot([
-    { path: 'wmgc', component: WorldMapGraphComponent }
+    { path: 'wmgc', component: WorldMapGraphComponent },
+    { path: 'hrc', component: HashrateChartComponent },
+    { path: 'fbc', component: FoundBlocksChartComponent }
   ]),
   ],
   exports: [RouterModule]
--- src/app/hashrate-chart/hashrate-chart.component.ts
+++ src/app/hashrate-chart/hashrate-chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var AmCharts: any;
 
 @Component({
   selector: 'app-hashrate-chart',
@@ -7,9 +8,177 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HashrateChartComponent implements OnInit {
 
+  dataProvider = [
+    {
+      'year': '2000',
+      'cars': 1587,
+      'motorcycles': 650,
+      'bicycles': 121
+    }, {
+      'year': '1995',
+      'cars': 1567,
+      'motorcycles': 683,
+      'bicycles': 146
+    }, {
+      'year': '1996',
+      'cars': 1617,
+      'motorcycles': 691,
+      'bicycles': 138
+    }, {
+      'year': '1997',
+      'cars': 1630,
+      'motorcycles': 642,
+      'bicycles': 127
+    }, {
+      'year': '1998',
+      'cars': 1660,
+      'motorcycles': 699,
+      'bicycles': 105
+    }, {
+      'year': '1999',
+      'cars': 1683,
+      'motorcycles': 721,
+      'bicycles': 109
+    }, {
+      'year': '2000',
+      'cars': 1691,
+      'motorcycles': 737,
+      'bicycles': 112
+    }, {
+      'year': '2001',
+      'cars': 1298,
+      'motorcycles': 680,
+      'bicycles': 101
+    }, {
+      'year': '2002',
+      'cars': 1275,
+      'motorcycles': 664,
+      'bicycles': 97
+    }, {
+      'year': '2003',
+      'cars': 1246,
+      'motorcycles': 648,
+      'bicycles': 93
+    }, {
+      'year': '2004',
+      'cars': 1218,
+      'motorcycles': 637,
+      'bicycles': 101
+    }, {
+      'year': '2005',
+      'cars': 1213,
+      'motorcycles': 633,
+      'bicycles': 87
+    }, {
+      'year': '2006',
+      'cars': 1199,
+      'motorcycles': 621,
+      'bicycles': 79
+    }, {
+      'year': '2007',
+      'cars': 1110,
+      'motorcycles': 210,
+      'bicycles': 81
+    }, {
+      'year': '2008',
+      'cars': 1165,
+      'motorcycles': 232,
+      'bicycles': 75
+    }, {
+      'year': '2009',
+      'cars': 1145,
+      'motorcycles': 219,
+      'bicycles': 88
+    }, {
+      'year': '2010',
+      'cars': 1163,
+      'motorcycles': 201,
+      'bicycles': 82
+    }, {
+      'year': '2011',
+      'cars': 1180,
+      'motorcycles': 285,
+      'bicycles': 87
+    }, {
+      'year': '2012',
+      'cars': 1159,
+      'motorcycles': 277,
+      'bicycles': 71
+    }
+  ];
+  graphs = [
+    {
+      'fillAlphas': 0.5,
+      'lineAlpha': 0.5,
+      'title': 'Cars',
+      'valueField': 'cars',
+      'index': 2
+    }, {
+      'fillAlphas': 0.5,
+      'lineAlpha': 0.5,
+      'title': 'Motorcycles',
+      'valueField': 'motorcycles',
+      'index': 3
+    }, {
+      'fillAlphas': 0.5,
+      'lineAlpha': 0.5,
+      'title': 'Bicycles',
+      'valueField': 'bicycles',
+      'index': 1
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+
+    AmCharts.addInitHandler(function (chart) {
+      // Reorder chart graph's based on their "index" value
+      chart.graphs.sort(function (a, b) {
+        if (a.index == b.index) {
+          return 0;
+        }
+        return a.index < b.index ? -1 : 1;
+      });
+    }, ["serial"]);
+    /**
+     * Make the chart
+     */
+
+    let chart = AmCharts.makeChart('chartdiv', {
+      type: 'serial',
+      theme: 'light',
+      legend: {
+        align: 'center',
+        equalWidths: false,
+        periodValueText: 'total: [[value.sum]]',
+        valueAlign: 'left',
+        valueText: '[[value]] ([[percents]]%)',
+        valueWidth: 100
+      },
+      valueAxes: [{
+        stackType: 'regular',
+        gridAlpha: 0.07,
+        position: 'left',
+        title: 'percent'
+      }],
+      dataProvider: this.dataProvider,
+      graphs: this.graphs,
+      plotAreaBorderAlpha: 0,
+      marginLeft: 0,
+      marginBottom: 0,
+      chartCursor: {
+        cursorAlpha: 0,
+        zoomable: false
+      },
+      categoryField: 'year',
+      categoryAxis: {
+        startOnAxis: true,
+        axisColor: '#DADADA',
+        gridAlpha: 0.07
+      }
+    });
+
   }
 
 }
